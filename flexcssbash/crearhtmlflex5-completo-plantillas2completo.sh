636587bc324c980d3d98a7e31554c55af6e6ef72
NEBULA1978 <rpascualey@gmail.com>
2023-10-08 18:23:59 +0200

    Creando css con bash para modificar valores flex completo 2 completo 2

--- flexcssbash/crearhtmlflex5-completo-plantillas2completo.sh
+++ flexcssbash/crearhtmlflex5-completo-plantillas2completo.sh
@@ -21,51 +21,56 @@ get_custom_values() {
   read -r ALIGN_CONTENT
 }
 
-# Ejemplos predefinidos
-EXAMPLE_1_GRID_DISPLAY="flex"
-EXAMPLE_1_FLEX_DIRECTION="row"
-EXAMPLE_1_FLEX_WRAP="wrap"
-EXAMPLE_1_JUSTIFY_CONTENT="center"
-EXAMPLE_1_ALIGN_ITEMS="flex-start"
-EXAMPLE_1_ALIGN_CONTENT="space-between"
+# Función para generar el archivo HTML con CSS personalizado
+generate_html() {
+  cat <<EOF >flexbox_example.html
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.flex-container {
+  display: $GRID_DISPLAY;
+  flex-direction: $FLEX_DIRECTION;
+  flex-wrap: $FLEX_WRAP;
+  justify-content: $JUSTIFY_CONTENT;
+  align-items: $ALIGN_ITEMS;
+  align-content: $ALIGN_CONTENT;
+  height: 300px; /* Ajusta la altura según sea necesario */
+  border: 1px solid black; /* Agrega un borde para visualizar el contenedor */
+}
 
-EXAMPLE_2_GRID_DISPLAY="flex"
-EXAMPLE_2_FLEX_DIRECTION="column"
-EXAMPLE_2_FLEX_WRAP="wrap"
-EXAMPLE_2_JUSTIFY_CONTENT="flex-start"
-EXAMPLE_2_ALIGN_ITEMS="center"
-EXAMPLE_2_ALIGN_CONTENT="stretch"
-
-EXAMPLE_3_GRID_DISPLAY="inline-flex"
-EXAMPLE_3_FLEX_DIRECTION="row"
-EXAMPLE_3_FLEX_WRAP="no-wrap"
-EXAMPLE_3_JUSTIFY_CONTENT="flex-end"
-EXAMPLE_3_ALIGN_ITEMS="center"
-EXAMPLE_3_ALIGN_CONTENT="space-around"
-
-EXAMPLE_4_GRID_DISPLAY="inline-flex"
-EXAMPLE_4_FLEX_DIRECTION="column"
-EXAMPLE_4_FLEX_WRAP="wrap-reverse"
-EXAMPLE_4_JUSTIFY_CONTENT="center"
-EXAMPLE_4_ALIGN_ITEMS="stretch"
-EXAMPLE_4_ALIGN_CONTENT="flex-start"
-
-EXAMPLE_5_GRID_DISPLAY="flex"
-EXAMPLE_5_FLEX_DIRECTION="row-reverse"
-EXAMPLE_5_FLEX_WRAP="wrap"
-EXAMPLE_5_JUSTIFY_CONTENT="space-between"
-EXAMPLE_5_ALIGN_ITEMS="flex-start"
-EXAMPLE_5_ALIGN_CONTENT="flex-end"
+.flex-item {
+  width: 100px; /* Ancho de los elementos flexibles */
+  height: 100px; /* Altura de los elementos flexibles */
+  margin: 10px; /* Márgenes entre los elementos flexibles */
+  background-color: lightgray; /* Color de fondo de los elementos */
+}
+</style>
+</head>
+<body>
+<div class="flex-container">
+  <div class="flex-item">1</div>
+  <div class="flex-item">2</div>
+  <div class="flex-item">3</div>
+</div>
+</body>
+</html>
+EOF
+}
 
 while true; do
   clear
-  echo "Seleccione una opción de ejemplo de flexbox:"
-  echo "1. Ejemplo 1"
-  echo "2. Ejemplo 2"
-  echo "3. Ejemplo 3"
-  echo "4. Ejemplo 4"
-  echo "5. Ejemplo 5"
-  echo "6. Personalizar valores"
+  echo "Menú de Ejemplos de Flexbox:"
+  echo "1. Ejemplo de Flexbox con dirección 'row'"
+  echo "   - Elementos colocados horizontalmente de izquierda a derecha."
+  echo "2. Ejemplo de Flexbox con dirección 'row-reverse'"
+  echo "   - Elementos colocados horizontalmente de derecha a izquierda."
+  echo "3. Ejemplo de Flexbox con dirección 'column'"
+  echo "   - Elementos colocados verticalmente de arriba abajo."
+  echo "4. Ejemplo de Flexbox con dirección 'column-reverse'"
+  echo "   - Elementos colocados verticalmente de abajo arriba."
+  echo "5. Ejemplo de Flexbox con personalización de valores"
+  echo "   - Puedes configurar los valores de Flexbox a tu elección."
   echo "0. Salir"
 
   read -r OPTION
@@ -77,6 +82,7 @@ while true; do
       JUSTIFY_CONTENT="$EXAMPLE_1_JUSTIFY_CONTENT"
       ALIGN_ITEMS="$EXAMPLE_1_ALIGN_ITEMS"
       ALIGN_CONTENT="$EXAMPLE_1_ALIGN_CONTENT"
+      generate_html # Llamar a la función para generar HTML
       ;;
     2)
       GRID_DISPLAY="$EXAMPLE_2_GRID_DISPLAY"
@@ -85,6 +91,7 @@ while true; do
       JUSTIFY_CONTENT="$EXAMPLE_2_JUSTIFY_CONTENT"
       ALIGN_ITEMS="$EXAMPLE_2_ALIGN_ITEMS"
       ALIGN_CONTENT="$EXAMPLE_2_ALIGN_CONTENT"
+      generate_html # Llamar a la función para generar HTML
       ;;
     3)
       GRID_DISPLAY="$EXAMPLE_3_GRID_DISPLAY"
@@ -93,6 +100,7 @@ while true; do
       JUSTIFY_CONTENT="$EXAMPLE_3_JUSTIFY_CONTENT"
       ALIGN_ITEMS="$EXAMPLE_3_ALIGN_ITEMS"
       ALIGN_CONTENT="$EXAMPLE_3_ALIGN_CONTENT"
+      generate_html # Llamar a la función para generar HTML
       ;;
     4)
       GRID_DISPLAY="$EXAMPLE_4_GRID_DISPLAY"
@@ -101,6 +109,7 @@ while true; do
       JUSTIFY_CONTENT="$EXAMPLE_4_JUSTIFY_CONTENT"
       ALIGN_ITEMS="$EXAMPLE_4_ALIGN_ITEMS"
       ALIGN_CONTENT="$EXAMPLE_4_ALIGN_CONTENT"
+      generate_html # Llamar a la función para generar HTML
       ;;
     5)
       GRID_DISPLAY="$EXAMPLE_5_GRID_DISPLAY"
@@ -109,9 +118,11 @@ while true; do
       JUSTIFY_CONTENT="$EXAMPLE_5_JUSTIFY_CONTENT"
       ALIGN_ITEMS="$EXAMPLE_5_ALIGN_ITEMS"
       ALIGN_CONTENT="$EXAMPLE_5_ALIGN_CONTENT"
+      generate_html # Llamar a la función para generar HTML
       ;;
     6)
       get_custom_values
+      generate_html # Llamar a la función para generar HTML
       ;;
     0)
       exit 0
@@ -121,14 +132,6 @@ while true; do
       ;;
   esac
 
-  # Imprimir los valores seleccionados o personalizados
-  clear
-  echo "Valores seleccionados o personalizados:"
-  echo "Tipo de display para div: $GRID_DISPLAY"
-  echo "Dirección de los elementos flexibles: $FLEX_DIRECTION"
-  echo "Propiedad flex-wrap: $FLEX_WRAP"
-  echo "Propiedad justify-content: $JUSTIFY_CONTENT"
-  echo "Propiedad align-items: $ALIGN_ITEMS"
-  echo "Propiedad align-content: $ALIGN_CONTENT"
+  echo "Archivo HTML generado con las configuraciones seleccionadas o personalizadas. Puedes encontrarlo en flexbox_example.html."
   read -n 1 -s -r -p "Presiona cualquier tecla para volver al menú..."
 done
